3567ec662a738f7aae19dbc91c51605c11311e8b
Sam Kumar <samkumar99@gmail.com>
2016-06-07 23:24:49 -0700

    Implement tick computation for Time Axis

--- axis.cpp
+++ axis.cpp
@@ -1,9 +1,13 @@
 #include "axis.h"
 #include "stream.h"
 
+#include <algorithm>
 #include <cmath>
 
+#include <QDate>
+#include <QDateTime>
 #include <QPair>
+#include <QTimeZone>
 #include <QVector>
 
 Axis::Axis()
@@ -49,7 +53,7 @@ bool Axis::setDomain(float low, float high)
     return true;
 }
 
-QVector<QPair<double, QString>> Axis::getTicks()
+QVector<struct tick> Axis::getTicks()
 {
     int precision = (int) (0.5 + log10(this->domainHi - this->domainLo) - 1);
     double delta = pow(10, precision);
@@ -69,24 +73,24 @@ QVector<QPair<double, QString>> Axis::getTicks()
 
     double low = ceil(this->domainLo / delta) * delta;
 
-    QVector<QPair<double, QString>> ticks;
+    QVector<struct tick> ticks;
     ticks.reserve(MAXTICKS + 1);
 
     precision = -precision;
     if (precision >= 0)
     {
-        while (low < this->domainHi + delta / 10)
+        while (low < this->domainHi + delta / (MAXTICKS + 1))
         {
-            ticks.append(QPair<double, QString>(low, QString::number(low, 'e', precision)));
+            ticks.append({low, QString::number(low, 'e', precision)});
             low += delta;
         }
     }
     else
     {
         double power = pow(10, precision);
-        while (low < this->domainHi + delta / 10)
+        while (low < this->domainHi + delta / (MAXTICKS + 1))
         {
-            ticks.append(QPair<double, QString>(low, QString::number(round(low / power) * power, 'e', 0)));
+            ticks.append({low, QString::number(round(low / power) * power, 'e', 0)});
             low += delta;
         }
     }
@@ -102,4 +106,218 @@ float Axis::map(float x)
 float Axis::unmap(float y)
 {
     return this->domainLo + y * (this->domainHi - this->domainLo);
+}
+
+int64_t getTimeTickDelta(const int64_t* intervals, int len, int64_t span)
+{
+    Q_ASSERT(len > 0);
+    int64_t idealWidth = span / TIMEAXIS_MAXTICKS;
+    const int64_t* tickptr = std::lower_bound(intervals, intervals + len, idealWidth);
+    if (tickptr == intervals + len)
+    {
+        tickptr--;
+    }
+    return *tickptr;
+}
+
+QString getTimeTickLabel(int64_t timestamp, QDateTime& datetime, Timescale granularity)
+{
+    int64_t minute;
+
+    /* In this switch statement, we intentionally fall through in every
+     * case.
+     */
+    switch (granularity)
+    {
+    case Timescale::NANOSECOND:
+        if (timestamp % MILLISECOND_NS != 0)
+        {
+            int64_t nanos = timestamp % SECOND_NS;
+            if (nanos < 0)
+            {
+                nanos += SECOND_NS;
+            }
+            return QStringLiteral(".%1").arg(nanos, 9, 10, QChar('0'));
+        }
+    case Timescale::MILLISECOND:
+        if (datetime.time().msec() != 0)
+        {
+            return datetime.toString(".zzz");
+        }
+    case Timescale::SECOND:
+        if (datetime.time().second() != 0)
+        {
+            return datetime.toString(QStringLiteral(":ss"));
+        }
+    case Timescale::MINUTE:
+        minute = datetime.time().minute();
+        if (minute != 0)
+        {
+            int64_t hour = datetime.time().hour() % 12;
+            if (hour == 0)
+            {
+                hour = 12; // Midnight or Noon
+            }
+            return QStringLiteral("%1:%2").arg(hour, 2, 10).arg(minute, 2, 10, QChar('0'));
+        }
+    case Timescale::HOUR:
+        if (datetime.time().hour() != 0)
+        {
+            return datetime.toString(QStringLiteral("hh AP"));
+        }
+    case Timescale::DAY:
+        if (datetime.date().day() != 1)
+        {
+            return datetime.toString(QStringLiteral("ddd MMM dd"));
+        }
+    case Timescale::MONTH:
+        if (datetime.date().month() != 1)
+        {
+            return datetime.toString(QStringLiteral("MMMM"));
+        }
+    case Timescale::YEAR:
+        return datetime.toString(QStringLiteral("yyyy"));
+    }
+
+    Q_UNREACHABLE();
+}
+
+bool TimeAxis::setDomain(int64_t low, int64_t high)
+{
+    if (low >= high)
+    {
+        return false;
+    }
+
+    this->domainLo = low;
+    this->domainHi = high;
+    return true;
+}
+
+inline int ceildiv(const int x, const int y)
+{
+    Q_ASSERT(y > 0);
+    return (x / y) + ((x % y) > 0);
+}
+
+QVector<struct timetick> TimeAxis::getTicks()
+{
+    uint64_t delta = (uint64_t) (this->domainHi - this->domainLo);
+
+    int64_t deltatick;
+    Timescale granularity;
+
+    /* First find deltatick. In the case of months and years, which are
+     * of variable length, just find the number of months or years.
+     */
+    if (delta < MAX_NANOTICK * TIMEAXIS_MAXTICKS)
+    {
+        deltatick = getTimeTickDelta(NANOTICK_INTERVALS, NANOTICK_INTERVALS_LEN, delta);
+        granularity = Timescale::NANOSECOND;
+    }
+    else if(delta < MAX_MILLITICK * TIMEAXIS_MAXTICKS)
+    {
+        deltatick = getTimeTickDelta(MILLITICK_INTERVALS, MILLITICK_INTERVALS_LEN, delta);
+        granularity = Timescale::MILLISECOND;
+    }
+    else if (delta < MAX_SECTICK * TIMEAXIS_MAXTICKS)
+    {
+        deltatick = getTimeTickDelta(SECTICK_INTERVALS, SECTICK_INTERVALS_LEN, delta);
+        granularity = Timescale::SECOND;
+    }
+    else if (delta < MAX_MINUTETICK * TIMEAXIS_MAXTICKS)
+    {
+        deltatick = getTimeTickDelta(MINUTETICK_INTERVALS, MINUTETICK_INTERVALS_LEN, delta);
+        granularity = Timescale::MINUTE;
+    }
+    else if (delta < MAX_HOURTICK * TIMEAXIS_MAXTICKS)
+    {
+        deltatick = getTimeTickDelta(HOURTICK_INTERVALS, HOURTICK_INTERVALS_LEN, delta);
+        granularity = Timescale::HOUR;
+    }
+    else if (delta < MAX_DAYTICK * TIMEAXIS_MAXTICKS)
+    {
+        deltatick = getTimeTickDelta(DAYTICK_INTERVALS, DAYTICK_INTERVALS_LEN, delta);
+        granularity = Timescale::DAY;
+    }
+    else if (delta < MAX_MONTHTICK * TIMEAXIS_MAXTICKS)
+    {
+        deltatick = getTimeTickDelta(MONTHTICK_INTERVALS, MONTHTICK_INTERVALS_LEN, delta);
+        granularity = Timescale::MONTH;
+    }
+    else
+    {
+        deltatick = getTimeTickDelta(YEARTICK_INTERVALS, YEARTICK_INTERVALS_LEN, delta);
+        granularity = Timescale::YEAR;
+    }
+
+    QVector<struct timetick> ticks;
+
+    int64_t domainLoMSecs = this->domainLo / MILLISECOND_NS;
+    int64_t domainLoNSecs = this->domainLo % MILLISECOND_NS;
+    if (domainLoNSecs < 0)
+    {
+        domainLoMSecs -=1;
+        domainLoNSecs += MILLISECOND_NS;
+    }
+
+    int64_t starttime;
+
+    switch(granularity)
+    {
+    case Timescale::YEAR:
+    {
+        int yeardelta = (int) (deltatick / YEAR_NS);
+        Q_ASSERT((deltatick % YEAR_NS) == 0);
+
+        QDateTime date = QDateTime::fromMSecsSinceEpoch(domainLoMSecs - 1, QTimeZone::utc());
+        int curryear = ceildiv(date.date().year() + 1, yeardelta) * yeardelta;
+        date.setDate(QDate(curryear, 0, 0));
+
+        if (domainLoMSecs == date.toMSecsSinceEpoch() && domainLoNSecs > 0)
+        {
+            date = date.addYears(yeardelta);
+        }
+
+        while ((starttime = date.toMSecsSinceEpoch() * MILLISECOND_NS) <= this->domainHi)
+        {
+            ticks.append({ starttime, getTimeTickLabel(starttime, date, granularity) });
+            date = date.addYears(yeardelta);
+        }
+        break;
+    }
+    case Timescale::MONTH:
+    {
+        int monthdelta = (int) (deltatick / MONTH_NS);
+        Q_ASSERT((deltatick % MONTH_NS) == 0);
+
+        QDateTime date = QDateTime::fromMSecsSinceEpoch(domainLoMSecs - 1, QTimeZone::utc());
+        /* currmonth is an int from 0 to 11. */
+        int currmonth = ceildiv(date.date().month(), monthdelta) * monthdelta;
+        int curryear = date.date().year() + (currmonth / 12);
+        currmonth %= 12;
+        date.setDate(QDate(curryear, currmonth + 1, 0));
+        if (this->domainLo == date.toMSecsSinceEpoch() && this->domainLo > 0)
+        {
+            date = date.addMonths(monthdelta);
+        }
+        while ((starttime = date.toMSecsSinceEpoch() * MILLISECOND_NS) <= this->domainHi)
+        {
+            ticks.append({ starttime, getTimeTickLabel(starttime, date, granularity) });
+            date = date.addMonths(monthdelta);
+        }
+        break;
+    }
+    default:
+        starttime = ((this->domainLo / deltatick) + (this->domainLo % deltatick != 0)) * deltatick;
+        while (starttime <= this->domainHi) {
+            // Add the tick to ticks
+            QDateTime date = QDateTime::fromMSecsSinceEpoch(ceildiv(starttime, MILLISECOND_NS));
+            ticks.append({ starttime, getTimeTickLabel(starttime, date, granularity) });
+            starttime += deltatick;
+        }
+        break;
+    }
+
+    return ticks;
 }
--- axis.h
+++ axis.h
@@ -3,6 +3,8 @@
 
 #include "stream.h"
 
+#include <cstdint>
+
 #include <QList>
 #include <QPair>
 #include <QString>
@@ -13,6 +15,12 @@
 
 /* Both Stream and Axis need declarations of each other. */
 class Stream;
+
+struct tick
+{
+    double value;
+    QString label;
+};
 
 class Axis
 {
@@ -33,12 +41,12 @@ public:
      */
     bool rmStream(Stream* s);
 
-    /* Sets the domain of this axis to be the range [low, high].
+    /* Sets the domain of this axis to be the interval [low, high].
      * Returns true on success and false on failure.
      */
     bool setDomain(float low, float high);
 
-    QVector<QPair<double, QString>> getTicks();
+    QVector<struct tick> getTicks();
 
 private:
     /* Maps a floating point number in the domain of this axis to a
@@ -57,6 +65,93 @@ private:
     QList<Stream*> streams;
 
     QString name;
+};
+
+/* Milliseconds, since that's what we need to work with QDateTime... */
+const int64_t MILLISECOND_MS = 1LL;
+const int64_t SECOND_MS = 1000LL * MILLISECOND_MS;
+const int64_t MINUTE_MS = 60LL * SECOND_MS;
+const int64_t HOUR_MS = 60LL * MINUTE_MS;
+const int64_t DAY_MS = 24LL * HOUR_MS;
+const int64_t YEAR_MS = (int64_t) (0.5 + 365.24 * DAY_MS); // We're within the precision of a double
+const int64_t MONTH_MS = YEAR_MS / 12LL;
+
+/* Nanoseconds, since that's often more convenient when we have int64_ts. */
+const int64_t NANOSECOND_NS = 1LL;
+const int64_t MILLISECOND_NS = 1000000LL;
+const int64_t SECOND_NS = SECOND_MS * MILLISECOND_NS;
+const int64_t MINUTE_NS = MINUTE_MS * MILLISECOND_NS;
+const int64_t HOUR_NS = HOUR_MS * MILLISECOND_NS;
+const int64_t DAY_NS = DAY_MS * MILLISECOND_NS;
+const int64_t YEAR_NS = YEAR_MS * MILLISECOND_NS;
+const int64_t MONTH_NS = MONTH_MS * MILLISECOND_NS;
+
+#define TIMEAXIS_MAXTICKS 7
+// I should multiply each element by NANOSECOND_NS... but I'm going to take a shortcut here since it is just 1.
+const int64_t NANOTICK_INTERVALS[18] = { 1LL, 2LL, 5LL, 10LL, 20LL, 50LL, 100LL, 200LL, 500LL, 1000LL, 2000LL, 5000LL, 10000LL, 20000LL, 50000LL, 100000LL, 200000LL, 500000LL };
+const int NANOTICK_INTERVALS_LEN = 18;
+const int64_t MAX_NANOTICK = 500000LL * NANOSECOND_NS;
+const int64_t MILLITICK_INTERVALS[9] = { 1LL * MILLISECOND_NS, 2LL * MILLISECOND_NS, 5LL * MILLISECOND_NS, 10LL * MILLISECOND_NS, 20LL * MILLISECOND_NS, 50LL * MILLISECOND_NS, 100LL * MILLISECOND_NS, 200LL * MILLISECOND_NS, 500LL * MILLISECOND_NS };
+const int MILLITICK_INTERVALS_LEN = 9;
+const int64_t MAX_MILLITICK = 500LL * MILLISECOND_NS;
+const int64_t SECTICK_INTERVALS[6] = { 1LL * SECOND_NS, 2LL * SECOND_NS, 5LL * SECOND_NS, 10LL * SECOND_NS, 20LL * SECOND_NS, 30LL * SECOND_NS };
+const int SECTICK_INTERVALS_LEN = 6;
+const int64_t MAX_SECTICK = 30LL * SECOND_NS;
+const int64_t MINUTETICK_INTERVALS[6] = { 1LL * MINUTE_NS, 2LL * MINUTE_NS, 5LL * MINUTE_NS, 10LL * MINUTE_NS, 20LL * MINUTE_NS, 30LL * MINUTE_NS };
+const int MINUTETICK_INTERVALS_LEN = 6;
+const int64_t MAX_MINUTETICK = 30LL * MINUTE_NS;
+const int64_t HOURTICK_INTERVALS[6] = { 1LL * HOUR_NS, 2LL * HOUR_NS, 3LL * HOUR_NS, 4LL * HOUR_NS, 6LL * HOUR_NS, 12LL * HOUR_NS };
+const int HOURTICK_INTERVALS_LEN = 6;
+const int64_t MAX_HOURTICK = 12LL * HOUR_NS;
+const int64_t DAYTICK_INTERVALS[5] = { 1LL * DAY_NS, 2LL * DAY_NS, 4LL * DAY_NS, 7LL * DAY_NS, 14LL * DAY_NS };
+const int DAYTICK_INTERVALS_LEN = 5;
+const int64_t MAX_DAYTICK = 14LL * DAY_NS;
+const int64_t MONTHTICK_INTERVALS[4] = {1LL * MONTH_NS, 2LL * MONTH_NS, 3LL * MONTH_NS, 6LL * MONTH_NS};
+const int MONTHTICK_INTERVALS_LEN = 4;
+const int64_t MAX_MONTHTICK = 6LL * MONTH_NS;
+const int64_t YEARTICK_INTERVALS[10] = { 1LL * YEAR_NS, 2LL * YEAR_NS, 5LL * YEAR_NS, 10LL * YEAR_NS, 20LL * YEAR_NS, 50LL * YEAR_NS, 100LL * YEAR_NS, 200LL * YEAR_NS };
+const int YEARTICK_INTERVALS_LEN = 10;
+const int64_t MAX_YEARTICK = 200LL * YEAR_NS;
+
+enum class Timescale
+{
+    NANOSECOND,
+    MILLISECOND,
+    SECOND,
+    MINUTE,
+    HOUR,
+    DAY,
+    MONTH,
+    YEAR
+};
+
+struct timetick
+{
+    int64_t timeval;
+    QString label;
+};
+
+/* Given the span of this time axis in a given unit, and a list of possible
+ * tick spacings in that same unit, chooses the most appropriate spacing
+ * between ticks.
+ */
+int64_t getTimeTickDelta(const int64_t* intervals, int intervals_len, int64_t span);
+
+class TimeAxis
+{
+public:
+    TimeAxis();
+
+    /* Sets the domain of this axis to be the interval [low, high].
+     * Returns true on success and false on failure.
+     */
+    bool setDomain(int64_t low, int64_t high);
+
+    QVector<struct timetick> getTicks();
+
+private:
+    int64_t domainLo;
+    int64_t domainHi;
 };
 
 #endif // AXIS_H
